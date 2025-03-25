36785eb49e722b4d13a298728da26a2595500747
tun82434 <tun82434@temple.edu>
2025-03-25 10:25:52 -0400

    Changed file structure, with landing page and home page

--- frontend/app/(auth)/home/page.tsx
+++ frontend/app/(auth)/home/page.tsx
@@ -30,20 +30,50 @@ export default function Home() {
             </div>
             <h1 className="text-5xl font-bold font-mono">MedMemo</h1>
           </div>
+
+          {/* Dropdown Menu */}
+          <div className="relative">
+            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} 
+            className="px-4 py-2 bg-white text-red-500 font-bold rounded-md hover:bg-gray-200 transition">
+              Menu ▼
+            </button>
+
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-2 w-40 bg-white rounded-md shadow-lg">
+                <a href="/" className={defaultButtonStyle}>Home</a>
+                <a href="/about" className={defaultButtonStyle}>About</a>
+                <a href="/Account" className={defaultButtonStyle}>Account</a>
+                <button onClick={handleLogin} className="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-200 transition">
+                  Login
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </nav>
 
       <div className="flex flex-grow">
-
+        {/* Sidebar */}
+        <div
+          className="w-64 text-white p-4 overflow-y-auto h-screen"
+          style={{ backgroundColor: "#E0DFDF" }}
+        >
+          <h2 className="text-xl font-bold text-center mb-6 text-gray-800">
+            Sidebar Title
+          </h2>
+          <p className="mb-4 text-center text-gray-600">
+            This is a scrollable sidebar.
+          </p>
+        </div>
 
         {/* Main Content */}
         <main className="flex-1 p-8">
           <div className="text-center">
             <h1 className="text-3xl font-bold text-black">
-              Welcome to the Landing Page
+              Welcome to the Homepage
             </h1>
             <p className="mt-2 text-gray-600">
-              This is a simple Landing Page with a navbar
+              This is a simple homepage with a sidebar beside the navbar.
             </p>
           </div>
         </main>
