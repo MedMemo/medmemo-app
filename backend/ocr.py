5cc14de7ed5225d1e2362dc897246af7a8f95958
Phuykong <m.phuykong@gmail.com>
2025-03-26 22:58:32 -0400

    connected to Azure

--- backend/ocr.py
+++ backend/ocr.py
@@ -6,6 +6,9 @@ from azure.ai.documentintelligence.models import DocumentAnalysisFeature, Analyz
 
 from dotenv import load_dotenv
 
+from pypdf import PdfReader, PdfWriter
+from pypdf.annotations import Rectangle
+
 import os
 
 # Load environment variables
@@ -22,11 +25,9 @@ def upload():
     credential = AzureKeyCredential(AZURE_KEY)
     document_intelligence_client = DocumentIntelligenceClient(AZURE_ENDPOINT,
                                                         credential)
-    print("request",request.files)
 
     if request.files:
         file = request.files['file']
-
         file_bytes = file.read()
         poller = document_intelligence_client.begin_analyze_document(
             "prebuilt-layout",
@@ -37,7 +38,68 @@ def upload():
     else:
         return jsonify({"message": "No file uploaded"}), 400
 
-    print(result)
+    reader = PdfReader(file)
+    page = reader.pages[0]
+    writer = PdfWriter()
+    writer.add_page(page)
+
+    # # Add the rectangle
+    # annotation = Rectangle(
+    #     rect=(50, 100, 200, 200),
+    #     #
+    # )
+    # writer.add_annotation(page_number=0, annotation=annotation)
+
+
+    if result['keyValuePairs']:
+        for pair in result['keyValuePairs']:
+
+            key = pair['key']
+            value = pair['value']
+
+            print(key["boundingRegions"][0]["polygon"])
+
+            # key_annotation = Rectangle(
+            #     rect=(key['boundingRegions'][0]['polygon'][6],
+            #             key['boundingRegions'][0]['polygon'][7],
+            #             key['boundingRegions'][0]['polygon'][2],
+            #             key['boundingRegions'][0]['polygon'][3]),
+            #     interior_color="ffaac00"
+            #     )
+            # key_annotation = Rectangle(
+            #     rect=((key['boundingRegions'][0]['polygon'][6],key['boundingRegions'][0]['polygon'][7]), # lower left
+            #         (key['boundingRegions'][0]['polygon'][4],key['boundingRegions'][0]['polygon'][5]), # lower right
+            #         (key['boundingRegions'][0]['polygon'][0], key['boundingRegions'][0]['polygon'][1]), # upper left
+            #         (key['boundingRegions'][0]['polygon'][2], key['boundingRegions'][0]['polygon'][3])), # upper right
+
+            #     interior_color="ffaac00"
+            #     )
+            key_annotation = Rectangle(
+                rect=(50, 51, 200, 201),
+                interior_color="ffaac00"
+
+            )
+            print("key",(key['boundingRegions'][0]['polygon'][6],key['boundingRegions'][0]['polygon'][7]), # lower left
+                    (key['boundingRegions'][0]['polygon'][4],key['boundingRegions'][0]['polygon'][5]), # lower right
+                    (key['boundingRegions'][0]['polygon'][0], key['boundingRegions'][0]['polygon'][1]), # upper left
+                    (key['boundingRegions'][0]['polygon'][2], key['boundingRegions'][0]['polygon'][3]))
+
+
+
+            writer.add_annotation(page_number=key['boundingRegions'][0]['pageNumber'] - 1,
+                                annotation=key_annotation)
+
+
+            print("Key:", pair['key'])
+            print("Value:", pair['value'])
+            print("Confidence:", pair['confidence'])
+            # print("Text content:")
+            # for content in pair['content']:
+            #     print(content)
+            print("--------------------------------------------------")
+
+    with open("annotated-pdf.pdf", "wb") as fp:
+        writer.write(fp)
 
 
     return jsonify({"message": "Upload endpoint"}), 200
