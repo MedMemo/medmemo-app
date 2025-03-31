9636461e2fab538baa3a67134155b96bd1c8b50a
Martin Duong <tun82434@temple.edu>
2025-03-31 17:54:55 -0400

    Made components for 3 types of Navigation Bar w/ Logo

---
+++ frontend/components/NavLogo_LogoOnly.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+
+
+const NavLogo = () => {
+
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push('/');
+  };
+
+    return (
+        <nav style={{ backgroundColor: "#CF4051" }} className="text-white p-4">
+        <div className="flex justify-between items-center w-full">
+          {/* Logo and Title */}
+          <div className="flex items-center cursor-pointer space-x-8" onClick={handleRedirect}>
+            <div className="bg-white p-1 rounded">
+              <img
+                src="/images/medmemo_logo.png"
+                alt="MedMemo Logo"
+                className="h-20 w-20"/>
+            </div>
+            <h1 className="text-5xl font-bold font-mono">MedMemo</h1>
+          </div>
+        </div>
+      </nav>
+    );
+    
+};
+
+export default NavLogo;
