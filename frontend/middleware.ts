2a8858676ec29c0301a68dd0672dbf7f9ba01c0a
tun82434 <tun82434@temple.edu>
2025-04-08 16:47:18 -0400

    Added Comments

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,29 +1,25 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-    console.log ("Middleware Function Initiated")
     try {
         const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/auth/get_user", {
             credentials: "include"
         });
 
         if (!response.ok) {
-            const data = await response.json
-            console.log("response.ok is false:", data)
+            // User is not logged in & redirect to login page
             return NextResponse.redirect(new URL('/login', request.url));
         } 
 
+        // User is logged in & continue 
         return NextResponse.next();
 
     } catch (error) {
         console.log()
     }
-
 }
  
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: ['/home', '/upload', '/display']
 }
